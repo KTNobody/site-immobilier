6319a342ae1c765b808b53cc77eaf3ee643b4d8c
KTNobody <stevenclain470@gmail.com>
2025-04-18 09:52:18 +0000

    Correction du projectId Sanity

--- src/sanity.js
+++ src/sanity.js
@@ -1,7 +1,7 @@
 import { createClient } from "@sanity/client";
 
 export const client = createClient({
-  projectId: "h9v35xhq", // ← Ton vrai Project ID Sanity
+  projectId: "h9v35xhq",
   dataset: "production",
   apiVersion: "2023-01-01",
   useCdn: true,
